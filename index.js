feefb9f1558acc64cfd1f85e53f9935d3f5f0c73
Vic Novosad <vic.novosad.us@gmail.com>
2023-01-19 13:51:14 -0600

    random trends added

--- index.js
+++ index.js
@@ -2,68 +2,89 @@
 
 
 const Search = {
+    trends : ["mountains", "city", "country", "iceberg", "sky", "flowers","travel","house","business",
+                        "winter","water","beach","nature","coffee","dog","texture",
+                        "car","technology","landscape","money","office","forest","space","dark"],
     photo(UrlSearchValue) {
         const self = this;
         
         if (UrlSearchValue) {
             $(`#search-input`).val(UrlSearchValue);
             Url.clean();
+            self.clearContentContainer();
+            self.search(`https://api.pexels.com/v1/search/?page=1&per_page=18&query=${UrlSearchValue}`);
         }
-        console.log('step1');
-        
         $(document).ready(function () {
-            console.log('step2');
             $(`#search-form`).submit(function(e){
                 console.log('step3');
                 e.preventDefault(); //Prevent from submitting a form when clicking on a "Submit" button
                 self.clearContentContainer();
                 const $searchValue = $(`#search-input`).val()
                 Url.update(`search=${$searchValue}`);
-                $.ajax({
-                    type: "GET",
-                    url: `https://api.pexels.com/v1/search/?page=1&per_page=18&query=${$searchValue}`,
-                    // url: `https://api.pexels.com/v1/photos/102775`,
-                    // url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${$searchValue}&maxResults=18&key=AIzaSyAInRHshjJ1NB5VJUuvgI07BnfieWlWyNU`,
-                    dataType: 'json',
-                    headers: {'Authorization': PEXEL_API_KEY},
-                    success: function(data){
-                        // console.log(data);
-                        data.photos.forEach(function(photo) {
-                            self.createPhoto(photo).appendTo($(`#content-container`));
-                        });
-                    }
-                });
+                self.search(`https://api.pexels.com/v1/search/?page=1&per_page=18&query=${$searchValue}`);
             });
         });
         return this;
     },
     getHeaderPhoto(){
         const self = this;
+        const searchURL = `https://api.pexels.com/v1/search/?page=${self.randomNumber(1,3800)}&per_page=1&orientation=landscape&query=${this.trends[this.randomNumber(0,this.trends.length-1)]}`;
         $.ajax({
             type: "GET",
-            url: `https://api.pexels.com/v1/curated?orientation=landscape&page=1&per_page=15`,
+            url: searchURL,
             dataType: 'json',
             headers: {'Authorization': PEXEL_API_KEY},
             success: function(data){
                 // console.log(JSON.stringify(data));
-                const randomNumber = Math.floor(Math.random() * 16); // random number between 0 and 16
-                const randomPhotoUrl = data.photos[randomNumber].src.original;
-                $('header').css('background-image', `url(${randomPhotoUrl})`);
-                $('header .author-name-wrap span').text(data.photos[randomNumber].photographer);
-                data.photos.forEach(function(photo) {
-                    self.createPhoto(photo).appendTo($(`#content-container`));
-                });
+                $('header').css('background-image', `url(${data.photos[0].src.original})`);
+                $('header .author-name-wrap a').text(data.photos[0].photographer);
+                $('header .author-name-wrap a').attr("href", data.photos[0].photographer_url);
             }
         });
     },
+    getRandomTrendingPhotos(){
+        this.search(`https://api.pexels.com/v1/curated?page=1&per_page=18`);
+    },
     clearContentContainer(){
         $(`#content-container`).html('');
+    },
+    randomNumber(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max + 1);
+        return Math.floor(Math.random() * (max - min) + min); // max and min is inclusive
     },
     formatNumber(n) {
         if (n >= 1e9) return +(n / 1e9).toFixed(1) + "G";
         if (n >= 1e6) return +(n / 1e6).toFixed(1) + "M";
         if (n >= 1e3) return +(n / 1e3).toFixed(1) + "K";
         return n;
+    },
+    search(searchURL){
+        // searchURL =  `https://api.pexels.com/v1/photos/102775`; // search by photo ID
+        const self = this;
+        $.ajax({
+            type: "GET",
+            url: searchURL,
+            dataType: 'json',
+            headers: {'Authorization': PEXEL_API_KEY},
+            success: function(data){
+                // console.log(data);
+                data.photos.forEach(function(photo) {
+                    self.createPhoto(photo).appendTo($(`#content-container`));
+                });
+            }
+        });
+    },
+    getRandomIndexes(){
+        const randomIndices = [];
+        while (randomIndices.length < 5) {
+            const randomIndex = this.randomNumber(0, this.trends.length-1);
+            // console.log(randomIndex);
+            if (!randomIndices.includes(randomIndex)) {
+                randomIndices.push(randomIndex);
+            }
+        }
+        return randomIndices;
     },
     createPhoto(photo){
         const $currentPhoto = $(`
@@ -127,8 +148,14 @@ const Url = {
 };
 
 function Init(){
-
+    const self = this;
     // setActiveSelection($("#pages-nav li:first-child").addClass("active"));
+    
+    const randomIndices = Search.getRandomIndexes();
+    $(`#trending-bar li`).each(function(index, element) {
+        $(this).find('a').attr("href", `/?search=${Search.trends[randomIndices[index]]}`);
+        $(this).find('span.trend').text(`${Search.trends[randomIndices[index]]}`);
+    });
 
     $(`#pages-nav li`).click(function(e){
         $(`#pages-nav li`).removeClass('active');
@@ -148,17 +175,13 @@ function Init(){
         const sortBy = $(this).data("sort");
         $('#sort-by-btn h2').text(sortBy);
     });
+
+    Search.getHeaderPhoto();
+    if (Url.load()){
+        Search.photo(Url.load());
+    } else {
+        Search.getRandomTrendingPhotos();
+    }
 };
 
 window.onload = Init;
-const url = Url.load();
-if(url){
-    console.log('true');
-    Search.photo(url);
-} else {
-    console.log('false');
-    Search.getHeaderPhoto();
-    // Search.header(Url.load());
-    // Search.new(Url.load());
-}
-// Url.clean();
